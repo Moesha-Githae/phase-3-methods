65de6f027e5050659861521fb3f8076757c2134f
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-05-17 10:01:59 +0300

    complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "hello #{name}!"
+end
+
+def greet_with_default name='programmer'
+    puts "hello ,#{name}"
+end
+
+def add num1,num2
+    num1 + num2
+end
+
+def halve num1
+    return nill unless num.class == integer
+    num / 2
+end
